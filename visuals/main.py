a01b8ccd01e90bf5c6239b818051e2c802eaa04b
Shrey <sb15895@my.bristol.ac.uk>
2022-10-19 13:40:35 +0100

    added python graphing script

---
+++ visuals/main.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+filename = "/home/shrey/Coding/iocomp/test/run_dir/compute_write_time.csv"
+
+"""
+Read data
+"""
+mydata2 = pd.read_csv(
+filename,
+index_col=False,
+skiprows=0
+)
+
+"""
+Data analysis, ignore the first element 
+"""
+xAxis = mydata2.columns.values[1:] # header file 
+computeTime = mydata2.iloc[0].values[1:]
+totalTime = mydata2.iloc[1].values[1:]
+
+
+"""
+Bar plot
+"""
+X_axis = np.arange(len(xAxis))
+plt.bar(X_axis - 0.2,computeTime, 0.4)
+plt.bar(X_axis + 0.2,totalTime, 0.4)
+plt.title('iocomp comparison')
+plt.xlabel('STREAM benchmark category')
+plt.ylabel('Times(s)')
+plt.show()
