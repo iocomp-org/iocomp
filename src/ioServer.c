3c32c51fd26e4841a5d311620f8d42a9b065c175
Shrey <sb15895@my.bristol.ac.uk>
2022-11-07 17:16:55 +0000

    commented out MPI_Wait all

--- src/ioServer.c
+++ src/ioServer.c
@@ -55,8 +55,8 @@ void ioServer(struct iocomp_params *iocompParams)
 	// 		iocompParams->interComm, &request); 
 	mpi_error_check(ierr); 
 
-	ierr = 	MPI_Waitall(1, &request, &status); // wait for all processes to finish sending and recieving  
-	mpi_error_check(ierr); 
+	//ierr = 	MPI_Waitall(1, &request, &status); // wait for all processes to finish sending and recieving  
+	//mpi_error_check(ierr); 
 
 	// print out recieved data 
 #ifndef NDEBUG
