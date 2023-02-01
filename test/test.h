3ac540236f61d5ce05e3a29e0f4fd8a20ff9fa71
Shrey <sb15895@my.bristol.ac.uk>
2023-02-01 17:59:05 +0000

    updates

--- test/test.h
+++ test/test.h
@@ -1,7 +1,23 @@
 #include "mpi.h"
 #include "iocomp.h"
-
-double* initialise(struct iocomp_params* iocompParams, MPI_Comm comm); 
-void deleteFile(int ioname); 
+#define LOOPCOUNT 10 
+#define NDIM 3 
+struct test_variables{
+    int LOOP_COUNT; 
+	double startTime[LOOPCOUNT], endTime[LOOPCOUNT]; 
+	int localArraySize[NDIM]; 
+	size_t localDataSize; 
+	size_t globalDataSize; 
+	int myrank; 
+	int mysize;
+	int ioLibNum; 
+	char* FILENAMES[5];  
+}; 
+extern struct test_variables testParams; 
+double* initialise(struct iocomp_params* iocompParams, struct test_variables* testParams, MPI_Comm comm); 
+void deleteFile(struct test_variables* testParams); 
 int rmdir(char *path); // delete the directory 
+void rmfile(char* filename); // delete a file 
+void stats(struct test_variables* testParams); // stats function to output the write times, global size, local size, ranks 
+
 //int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf); 
