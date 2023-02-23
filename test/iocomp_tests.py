79136227d71640449647ad2638fe64e72fc9ee68
s2032563@ed.ac.uk <shrey.bhardwaj@ed.ac.uk>
2023-02-23 09:26:25 +0000

    logic complete

--- test/iocomp_tests.py
+++ test/iocomp_tests.py
@@ -14,13 +14,16 @@ NODESIZE = 128
 #globalSize=256
 
 # more than 1 full populated node 
-#globalSize=300
+globalSize=400
 
-# 2 full populated node 
-globalSize=512
+# multi full populated node 
+# globalSize=1024
 ranks = range(0,globalSize)
 i = 0 
-print("testing", globalSize%(NODESIZE)/2)
+
+numNodes = int(globalSize/(NODESIZE*2)) 
+lastwholeNode = numNodes *NODESIZE*2
+print("last whole node", lastwholeNode)
 
 for globalRank in ranks: 
     if (globalRank % (NODESIZE*2) == 0):
@@ -35,15 +38,14 @@ for globalRank in ranks:
 
     else: # size > 256 
         
-        lastwholeNode = int(globalSize/(NODESIZE*2)) *NODESIZE*2
         if(globalRank < lastwholeNode ): # globalRank comes within full node? 
-            if(globalRank < lastwholeNode/2):
-                print("comp", globalRank)
-            else:
-                print("io", globalRank)
-        
+            for x in range(0,numNodes+1,2): # go through all the nodes 
+                if(globalRank <  (x+1)*NODESIZE and globalRank >= (x)*NODESIZE):
+                    print("comp", globalRank)
+                if(globalRank >=  (x+1)*NODESIZE and globalRank < (x+2)*NODESIZE):
+                    print("io", globalRank)
+
         elif(globalRank >= lastwholeNode): # globalRank is not within full node. 
-             
             if(globalRank  < (globalSize + lastwholeNode)/2):
                 print("comp", globalRank)
             else:
