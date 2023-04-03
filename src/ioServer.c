70ce70158786d8cf5d9401c194ed046a7f1bb29e
Shrey <sb15895@my.bristol.ac.uk>
2023-04-03 13:35:08 +0100

    check for repeating initialisations

--- src/ioServer.c
+++ src/ioServer.c
@@ -11,8 +11,7 @@ void ioServer(struct iocomp_params *iocompParams)
 #ifndef NDEBUG
 	printf("ioServer -> started\n"); 
 #endif
-	int i, ierr; 
-	int ioRank; 
+	int i, ierr, ioRank; 
 	double timerStart, timerEnd; 
 	timerStart = timerEnd = 0.0; 
 
@@ -28,9 +27,7 @@ void ioServer(struct iocomp_params *iocompParams)
 	 * source is ioRank in interComm, as that is the 
 	 * rank of the paired ComputeProcess in computeComm 
 	 */ 
-
 	int source, tag; 
-
 	source = getPair(iocompParams); 
 	tag = source; 
 
@@ -40,6 +37,13 @@ void ioServer(struct iocomp_params *iocompParams)
 	int test_probe; 
 	int test_count = 1; 
 	int iter = 0; 
+
+	/*
+	 * initialise recv array 
+	 * previousCount to prevent repeated mallocs 
+	 */ 
+	double* recv = NULL; 
+	int previousCount = 0; 
 
 	/*
 	 * Check for ghost messages, for ever loop activated
@@ -64,6 +68,9 @@ void ioServer(struct iocomp_params *iocompParams)
 			ierr = MPI_Recv(&ghost, 0, MPI_INT, source, tag,
 					iocompParams->globalComm,&status);
 			mpi_error_check(ierr); 
+			// free recv array 
+			free(recv);
+			recv = NULL; 
 			break; 
 		}
 
@@ -72,12 +79,16 @@ void ioServer(struct iocomp_params *iocompParams)
 		 */ 
 		else if(test_count>0)
 		{
-			// initialise recv array 
-			double* recv = NULL; 
-			iocompParams->localDataSize = test_count; 
-			assert(iocompParams->localDataSize>0); // check for negative values 
-			recv = (double*)malloc(iocompParams->localDataSize*sizeof(double)); // one rank only sends to one rank
-			malloc_check(recv); 
+			// has it been initialised before?
+			if(test_count != previousCount) 
+			{
+				// initialise recv array 
+				iocompParams->localDataSize = test_count; 
+				assert(iocompParams->localDataSize>0); // check for negative values 
+				recv = (double*)malloc(iocompParams->localDataSize*sizeof(double)); // one rank only sends to one rank
+				malloc_check(recv); 
+				previousCount = test_count;  
+			} 
 
 #ifndef NDEBUG
 			printf("ioServer -> Initialisation of recv array with count %li \n", iocompParams->localDataSize); 
@@ -88,22 +99,20 @@ void ioServer(struct iocomp_params *iocompParams)
 
 #ifndef NDEBUG
 			printf("ioServer -> Recv data coming from rank %i \n",source ); 
-			//for(i = 0; i < (int)iocompParams->localDataSize; i++) { printf("%lf ",recv[i]); }// init size in each dimension to be n. For ex. NDIM = 2 will reslt in n x n }
-			printf("\n"); 
+			//for(i = 0; i < (int)iocompParams->localDataSize; i++) { printf("%lf ",recv[i]); }
+			// init size in each dimension to be n. For ex. NDIM = 2 will reslt in n x n }
 #endif
 
 			/*
 			 * Writing data using different IO libraries commences using io_libraries function
 			 * Parameters passed using iocompParams  
 			 */ 
-
 #ifndef NDEBUG
 			printf("ioServer -> Send to ioLibraries \n"); 
 #endif
 			ioLibraries(recv, iocompParams); 
-			free(recv);
-			recv = NULL; 
-	}  
-	iter++; 
-} 
+		}  
+
+		iter++; 
+	} 
 } 
