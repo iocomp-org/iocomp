6bf9220efe6f643a8962b3048612a09d48e44c0e
Shrey <sb15895@my.bristol.ac.uk>
2023-04-08 20:56:21 +0100

    reduced printing from highlowordering

--- src/highlowOrdering.c
+++ src/highlowOrdering.c
@@ -23,33 +23,6 @@
  */ 
 int sched_getcpu();
 
-/* This code is adapted from an example at:
- * http://brokestream.com/procstat.html */
-//int get_cpu_id()
-//{
-//  int i;
-//  FILE* procfile = fopen("/proc/self/stat", "r");
-//  long to_read = 8192;
-//  char buffer[to_read];
-//  int read = fread(buffer, sizeof(char), to_read, procfile);
-//	if(!read)
-//	{
-//		printf("error"); 
-//	}
-//  fclose(procfile);
-//
-//  // Field with index 38 (zero-based counting) is the one we want
-//  char* line = strtok(buffer, " ");
-//  for (i = 1; i < 38; i++)
-//  {
-//    line = strtok(NULL, " ");
-//  }
-//
-//  line = strtok(NULL, " ");
-//  int cpu_id = atoi(line);
-//  return cpu_id;
-//}
-
 void highlowOrdering(struct iocomp_params *iocompParams) 
 {
 	int globalRank, globalSize; 
@@ -65,17 +38,9 @@ void highlowOrdering(struct iocomp_params *iocompParams)
 	if(globalSize <= NODESIZE*2)
 	{
 		if(globalRank < globalSize/2){
-#ifdef PRINT_ORDERING
-			printf("COMP colour rank %i out of %i size on cpu_id %02d of node %s\n", 
-				globalRank, globalSize,sched_getcpu(),nodeName); 
-#endif
 			iocompParams->colour = compColour; 
 		} 
 		else{
-#ifdef PRINT_ORDERING
-			printf("IO colour rank %i out of %i size on cpu_id %02d of node %s\n", 
-				globalRank, globalSize,sched_getcpu(),nodeName); 
-#endif
 			iocompParams->colour = ioColour; 
 		}
 	} 
@@ -87,17 +52,9 @@ void highlowOrdering(struct iocomp_params *iocompParams)
 			for(int x = 0; x <= numNodes; x+=2 ) 
 			{
 				if(globalRank <  (x+1)*NODESIZE && globalRank >= (x)*NODESIZE){
-#ifdef PRINT_ORDERING
-					printf("COMP colour rank %i out of %i size on cpu_id %02d of node %s\n", 
-						globalRank, globalSize,sched_getcpu(),nodeName); 
-#endif
 					iocompParams->colour = compColour; 
 				} 
 				if(globalRank >=  (x+1)*NODESIZE && globalRank < (x+2)*NODESIZE){
-#ifdef PRINT_ORDERING
-					printf("IO colour rank %i out of %i size on cpu_id %02d of node %s\n", 
-						globalRank, globalSize,sched_getcpu(),nodeName); 
-#endif
 					iocompParams->colour = ioColour; 
 				} 
 			}
@@ -105,17 +62,9 @@ void highlowOrdering(struct iocomp_params *iocompParams)
 
 		else if(globalRank >= lastwholeNode){ // globalRank is not within full node. 
 			if(globalRank  < (globalSize + lastwholeNode)/2){
-#ifdef PRINT_ORDERING
-					printf("COMP colour rank %i out of %i size on cpu_id %02d of node %s\n", 
-						globalRank, globalSize,sched_getcpu(),nodeName); 
-#endif
 				iocompParams->colour = compColour; 
 			} 
 			else{
-#ifdef PRINT_ORDERING
-					printf("IO colour rank %i out of %i size on cpu_id %02d of node %s\n", 
-						globalRank, globalSize,sched_getcpu(),nodeName); 
-#endif
 				iocompParams->colour = ioColour; 
 			} 
 
