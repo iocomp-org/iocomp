891891b6afe1424a34bcd92721f58437626fab20
Shrey <sb15895@my.bristol.ac.uk>
2023-04-19 12:26:24 +0100

    used NDEBUG instead of verbose2 datawait

--- src/dataWait.c
+++ src/dataWait.c
@@ -10,13 +10,13 @@ void dataWait(struct iocomp_params *iocompParams,MPI_Request *request)
 
 	if(iocompParams->hyperthreadFlag) // only implement if HT flag switched on 
 	{
-		int ierr = MPI_Wait(request, MPI_STATUS_IGNORE); // wait for the data to be sent from previous dataSend 
-		mpi_error_check(ierr); 
 		int compRank; 
 		MPI_Comm_rank(iocompParams->compServerComm, &compRank); 
-#ifdef VERBOSE_2
+#ifdef NDEBUG
 		VERBOSE_1(compRank,"dataWait -> MPI wait activated\n"); 
 #endif
+		int ierr = MPI_Wait(request, MPI_STATUS_IGNORE); // wait for the data to be sent from previous dataSend 
+		mpi_error_check(ierr); 
 	}
 
 } 
