9dc099a01b48df1183db8e4cc11b286617734710
Shrey <sb15895@my.bristol.ac.uk>
2023-10-03 13:01:43 +0100

    new file predatasend to combine sending MPI bcasts and starting shared window

---
+++ src/preDataSend.c
@@ -0,0 +1,50 @@
+#include "../include/iocomp.h"
+#include "assert.h" 
+
+void preDataSend(struct iocomp_params *iocompParams, double* array)
+{
+
+	if(iocompParams->sharedFlag)
+	{
+#ifndef NDEBUG 
+		fprintf(iocompParams->debug, "dataSend -> Before MPI bcast, iocompParams->wintestflags"); 
+		for(int i = 0; i < NUM_WIN; i++)
+		{
+			fprintf(iocompParams->debug, "[%i]", 
+					iocompParams->wintestflags[i]); 
+		} 
+		fprintf(iocompParams->debug, "\n"); 
+#endif 
+
+		// check if test flags have a value between -1 and 4 before broadcasting 
+		for(int i = 0; i < NUM_WIN; i++)
+		{
+			assert(iocompParams->wintestflags[i] > -2); 
+			assert(iocompParams->wintestflags[i] < 5); 
+		}
+
+		// broadcast them to ioserver 
+		MPI_Bcast( iocompParams->wintestflags, NUM_WIN, MPI_INT, 0, iocompParams->newComm); 
+#ifndef NDEBUG 
+		fprintf(iocompParams->debug, "dataSendStart -> After MPI broadcast  \n "); 
+#endif 
+
+		// get the window test flag for the array 
+		for(int i = 0; i < NUM_WIN; i++)
+		{
+			if(iocompParams->array[i] == array)
+			{
+				// match array with windows and issue win complete 
+#ifndef NDEBUG 
+				fprintf(iocompParams->debug, "dataSendStart -> Before mpi win start for window %i \n ", i); 
+#endif 
+				int ierr = MPI_Win_start(iocompParams->group, 0, iocompParams->winMap[i]); 
+				mpi_error_check(ierr); 
+#ifndef NDEBUG 
+				fprintf(iocompParams->debug, "dataSendStart -> After mpi win start for window %i \n ", i); 
+#endif 
+			}
+		}
+	}
+} 
+
