6ea85b3c7b288f688cda788587e7f209b13affc3
Shrey <sb15895@my.bristol.ac.uk>
2023-10-29 12:24:40 +0000

    had to half cores to get test scripts working if comms are being split

--- test/runtestscript.sh
+++ test/runtestscript.sh
@@ -8,6 +8,7 @@ setup() {
 	mkdir -p ${TEST_DIR} 
 	cd ${TEST_DIR} 
 	cp ${CONFIG_FILE} . 
+	echo Testing in directory:; echo ${TEST_DIR} 
 } 
 
 # MPI cores
@@ -16,14 +17,18 @@ CORES_END=4
 
 # controls NX and NY parameters
 ARRAY_START=2
-ARRAY_END=3
+ARRAY_END=2
 
 # IO libraries selection 
-IO_START=3
+IO_START=0
 IO_END=3
 
+# flag
+#FLAG=HT
+FLAG=shared
+
 # STREAM tests in HT flag 
-echo 'Testing HT configuration'
+echo 'Testing ' ${FLAG} ' configuration'
 for CORES in $(seq ${CORES_START} 2 ${CORES_END});
 do 
 	for i in $(seq ${ARRAY_START} ${ARRAY_END});
@@ -35,10 +40,18 @@ do
 			echo "Testing for ${CORES} cores, array size ${NX} x ${NY}, IO lib ${IO}"
 			TEST_DIR=${TEST_HOME}/${CORES}/${NX}/${NY}/${IO}
 			setup
-			mpirun.mpich -n ${CORES} ${STREAM_EXE} --nx ${NX} --ny ${NY} --io ${IO} --HT 
-			sleep 1
-			mpirun.mpich -n ${CORES} ${TEST_EXE} --nx ${NX} --ny ${NY} --io ${IO} 
-			sleep 1 
+			mpirun.mpich -n ${CORES} ${STREAM_EXE} --nx ${NX} --ny ${NY} --io ${IO} --${FLAG} 
+			sleep 5
+			echo testing in; echo ${TEST_DIR}
+			# if flag activated then only half ranks are writing and global size is
+			# also halved. 
+			if [ -n ${FLAG} ]; then 
+				TEST_CORES=$((${CORES}/2))
+			else
+				TEST_CORES=${CORES}
+			fi 
+			mpirun.mpich -n ${TEST_CORES} ${TEST_EXE} --nx ${NX} --ny ${NY} --io ${IO} --v 
+			sleep 5
 		done 
 	done
 done 
