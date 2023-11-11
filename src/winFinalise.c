2dc50d7e0500899aa9a4f87a7f8bf0b2e46e8200
Shrey <sb15895@my.bristol.ac.uk>
2023-11-11 10:10:12 +0000

    added finalise and testinfo to match merge branch

---
+++ src/winFinalise.c
@@ -0,0 +1,14 @@
+#include "../include/iocomp.h"
+// if shared MPI used free window frees the arrays. Not the case with the HT
+// flag switched on  
+void winFinalise(struct iocomp_params *iocompParams)
+{
+	if(iocompParams->hyperthreadFlag == true)
+	{
+		for(int i = 0; i < iocompParams->numWin; i++)
+		{
+			free(iocompParams->array[i]); 
+			iocompParams->array[i] = NULL; 
+		} 
+	} 
+} 
