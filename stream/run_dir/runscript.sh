1e12f53ddaf4c5c9fcd086f50d9f835db9578a21
sb15895 <s2032563@ed.ac.uk>
2023-11-30 19:29:30 +0000

    updated running scripts. Now using options for the mapping, and functions in runscript

--- stream/run_dir/runscript.sh
+++ stream/run_dir/runscript.sh
@@ -5,13 +5,17 @@ callWeakScaling () {
     export PPN=128
     export NX=$((2**11))
     export NY=$((2**11)) 
-    export NODE_START=0
-    export NODE_END=0
+    export NODE_START=3
+    export NODE_END=4
     export IO_START=0
-    export IO_END=0
+    export IO_END=3
     export ARRAY="0"
-    export TIME="00:10:00"
-    export DIR=OUTPUT/v2.0.0/WEAK/100COMPUTE
+    # export TIME="01:30:00"
+    export MAP=0
+    export CASE_START=1
+    export CASE_END=5
+    export DIR=CRAY_PAT/100COMPUTE/WEAK
+    export MAP=0
     sh ./weakScaling.sh
   ) 
 }
@@ -19,14 +23,17 @@ callWeakScaling () {
 callStrongScaling () {
   (
     export PPN=128
-    export NX=$((2**14))
-    export NY=$((2**11)) 
-    export NODE_START=0
-    export NODE_END=0
-    export IO_START=0
-    export IO_END=0
-    export ARRAY="0"
-    export TIME="00:10:00"
+    export NX=$((2**12))
+    export NY=$((2**12)) 
+    export NODE_START=5
+    export NODE_END=5
+    export IO_START=2
+    export IO_END=2
+    export ARRAY="0-2"
+    export TIME="01:00:00"
+    export CASE_START=4
+    export CASE_END=5
+    export MAP=0
     export DIR=OUTPUT/v2.0.0/STRONG/GLOBALSIZE_8GB/100COMPUTE
     sh strongScaling.sh
   )
@@ -44,6 +51,7 @@ callTest () {
     export ARRAY="0"
     export TIME="00:10:00"
     export DIR=TESTING
+    export MAP=0
     sh strongScaling.sh
   )
 } 
@@ -51,27 +59,32 @@ callTest () {
 callMAP() {
   (
     export PPN=128
-    export NX=$((2**11))
+    export NX=$((2**14))
     export NY=$((2**11)) 
     export NODE_START=0
     export NODE_END=0
-    export IO_START=1
-    export IO_END=1
     export ARRAY="0"
-    export TIME="00:10:00"
-    export DIR=MAP_PROFILES
+    export TIME="01:00:00"
+    export DIR=MAP_PROFILES/100COMPUTE/STRONG # or weak 
     export MAP=1
-    # loop over cases 1 by 1 for MAP 
-    for case in $(seq 1 5)
+    # loop over IO layers for MAP from 0 to 3 
+    for io in $(seq 2 3)
     do 
-      export CASE_START=${case}
-      export CASE_END=${case}
-      sh ./weakScaling.sh
+      export IO_START=${io}
+      export IO_END=${io} 
+      # loop over cases 1 by 1 for MAP # from 1 - 5 
+      for case in $(seq 1 5)
+      do 
+        export CASE_START=${case}
+        export CASE_END=${case}
+        # sh ./weakScaling.sh
+        sh ./strongScaling.sh
+      done 
     done 
   ) 
 }
 
-callMAP
+# callMAP
 # callWeakScaling
-# callStrongScaling 
+callStrongScaling 
 # callTest 
--- stream/run_dir/strongScaling.sh
+++ stream/run_dir/strongScaling.sh
@@ -2,6 +2,11 @@
 
 TIMES=("5:00:00" "04:00:00" "03:00:00" "03:00:00" "03:00:00" "03:00:00" "03:00:00") 
 MAX_NODES=1
+
+# so that global size is always constant, irrespective of what is NODE start 
+NX_LOCAL=$((${NX}*2/(2**${NODE_START}))) # multiplying by 2 cos strong scaling halves the array for remainder 0.  
+NY_LOCAL=${NY}
+iter=${NODE_START}  
 # iterate through number of nodes 
 for i in $(seq ${NODE_START} ${NODE_END}) # 1 till 16 nodes 
 do 
@@ -13,14 +18,27 @@ do
     TIME_VAR=${TIMES[${i}]} 
   fi 
 
-  # get local values from the global specified for the number of nodes given 
+  # get local values from the global specified for the number of nodes given. 
+  # balance arrays so that no array is less than 16
   NUM_NODES=$((2**${i}))
-  NX_LOCAL=$((${NX}*${MAX_NODES}/${NUM_NODES})) 
-  FILESIZE_LOCAL=$((${NX_LOCAL} * ${NY} * 8/ 2**20)) 
-  FILESIZE_GLOBAL=$(( ${NX_LOCAL} * ${NY} * 8 * ${NUM_NODES} * 128 / (2*2**20) ))  # global size is halfed due to IO servers
 
-  echo NODES ${NUM_NODES} ARRAY SIZE ${NX_LOCAL} x ${NY} Local size ${FILESIZE_LOCAL}MiB Global size ${FILESIZE_GLOBAL}MiB  TIME ${TIME_VAR} IO ${IO_START} to ${IO_END} JOBS ${ARRAY} MAP ${MAP} CASE ${CASE_START} to ${CASE_END}
+  if [[ $(( $iter % 2)) == 0 ]] ; 
+  then 
+    NX_LOCAL=$((${NX_LOCAL}/2 )) 
+  fi 
+  if [[ $(( $iter % 2)) == 1 ]] ; 
+  then 
+    NY_LOCAL=$((${NY_LOCAL}/2 )) 
+  fi 
   
-  sbatch --export=ALL,NX=${NX_LOCAL},NY=${NY},DIR=${DIR},IO_START=${IO_START},IO_END=${IO_END},FLAG=${FLAG},MAP=${MAP},CASE_START=${CASE_START},CASE_END=${CASE_END} --qos=standard --nodes=${NUM_NODES} --ntasks-per-node=${PPN} --time=${TIME_VAR} --array=${ARRAY}  archer2.slurm 
+  ((iter++))
+
+  FILESIZE_GLOBAL=$((${NX} * ${NY} * 8 * 128/ 2**20)) 
+  FILESIZE_LOCAL=$((${NX_LOCAL} * ${NY_LOCAL} * 8/ 2**20)) 
+  FILESIZE_GLOBAL_CALCULATED=$((${FILESIZE_LOCAL} * ${NUM_NODES} * ${PPN})) # 1/2 no. of compute processes
+
+  echo NODES ${NUM_NODES} ARRAY SIZE ${NX_LOCAL} x ${NY_LOCAL} Local size ${FILESIZE_LOCAL}MiB Global size correct ${FILESIZE_GLOBAL}MiB and calculated ${FILESIZE_GLOBAL_CALCULATED}MiB  TIME ${TIME_VAR} IO ${IO_START} to ${IO_END} JOBS ${ARRAY} MAP ${MAP} CASE ${CASE_START} to ${CASE_END}
+  
+  sbatch --export=ALL,NX=${NX_LOCAL},NY=${NY_LOCAL},DIR=${DIR},IO_START=${IO_START},IO_END=${IO_END},FLAG=${FLAG},MAP=${MAP},CASE_START=${CASE_START},CASE_END=${CASE_END} --qos=standard --nodes=${NUM_NODES} --ntasks-per-node=${PPN} --time=${TIME_VAR} --array=${ARRAY} archer2.slurm 
 done 
 
